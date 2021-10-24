6fe305483bc57c4a88357c6ba4896300e7c70cec
Stuart Ianna <Stuart.Ianna@saftehnika.com>
2021-10-24 22:39:52 +0930

    Add BLE device constants for advertising.

---
+++ include/constants/ble.hpp
@@ -0,0 +1,9 @@
+#ifndef __CONSTANTS_BLE_HPP
+#define __CONSTANTS_BLE_HPP
+
+#define BT_DEVICE_NAME        "IMU Device"
+#define BT_DEVICE_NAME_LENGTH (sizeof(BT_DEVICE_NAME) - 1)
+#define BT_MANF_DATA \
+  { 0xFF, 0xFF, 0x00 }
+
+#endif  // __CONSTANTS_BLE_HPP
