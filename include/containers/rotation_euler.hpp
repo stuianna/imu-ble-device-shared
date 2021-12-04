a813a6cf0e65138d6dccb7769ee9264faaff470b
stuianna <stuian@protonmail.com>
2021-12-04 20:53:43 +1100

    Add doxygen comments.

--- include/containers/rotation_euler.hpp
+++ include/containers/rotation_euler.hpp
@@ -5,6 +5,14 @@
 #include <cstdint>
 
 struct RotationEuler {
+  /**
+   * @brief Construct a new Rotation Euler object
+   *
+   * @details The euler angles for each axis have a range of -180 to 180 degrees.
+   * @param x The rotation in degrees around the x axis (roll)
+   * @param y The rotation in degrees around the y axis (pitch)
+   * @param z The rotatio in degreesn around the z axis (yaw)
+   */
   RotationEuler(const float x, const float y, const float z) : _x(_limitAngle(x)), _y(_limitAngle(y)), _z(_limitAngle(z)) {}
 
  private:
@@ -30,13 +38,46 @@ struct RotationEuler {
   }
 
  public:
+  /**
+   * @brief Get the rotation around the x axis (roll)
+   * @return constexpr float The rotation in degress.
+   */
   constexpr float x() { return _x; };
+  /**
+   * @brief Get the rotation around the y axis (pitch)
+   * @return constexpr float The rotation in degress.
+   */
   constexpr float y() { return _y; };
+  /**
+   * @brief Get the rotation around the z axis (yaw)
+   * @return constexpr float The rotation in degress.
+   */
   constexpr float z() { return _z; };
+  /**
+   * @brief Get the roll in degrees.
+   * @return constexpr float The rotation in degress.
+   */
   constexpr float roll() { return _x; };
+  /**
+   * @brief Get the pitch in degrees.
+   * @return constexpr float The rotation in degress.
+   */
   constexpr float pitch() { return _y; };
+  /**
+   * @brief Get the yaw in degrees.
+   * @return constexpr float The rotation in degress.
+   */
   constexpr float yaw() { return _z; };
+  /**
+   * @brief Get rotation (x, y, z) as a buffer of 12 bytes.
+   * @return byte buffer.
+   */
   constexpr uint8_t* bytes() { return _bytes; }
+  /**
+   * @brief Get the length of the pointer pointed to by bytes()
+   *
+   * @return constexpr size_t Length of the array.
+   */
   constexpr size_t length() { return 12; }
 };
 static_assert(sizeof(RotationEuler) == 12);
