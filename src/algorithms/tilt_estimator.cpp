16fb46ca79591b82529ad5c2b00a775b4fb3a781
stuianna <stuian@protonmail.com>
2021-12-06 21:39:05 +1100

    Fix tilt estimation calculation.

--- src/algorithms/tilt_estimator.cpp
+++ src/algorithms/tilt_estimator.cpp
@@ -8,8 +8,18 @@ RotationEuler TiltEstimator::orientation() {
 void TiltEstimator::update(const TriaxalReading& acc, const TriaxalReading& gyr, const TriaxalReading& mag) {
   (void)gyr;
   (void)mag;
-  auto x = Angle::radians(atan2(-acc.y(), acc.z()));
-  auto y = Angle::radians(atan2(acc.x(), acc.z()));
+  auto x = Angle::radians(-atan2(acc.y(), acc.z()));
+  auto y = Angle::radians(-atan2(-acc.x(), sqrt(acc.z() * acc.z() + acc.y() * acc.y())));
+
+  if(acc.z() < 0.f) {
+    if(acc.x() > 0.f) {
+      y = y.degrees(180 - y.degrees());
+    }
+    else {
+      y = y.degrees(-180 - y.degrees());
+    }
+  }
+
   _currentOrientation.x(x);
   _currentOrientation.y(y);
   _currentOrientation.z(Angle::radians(0));
